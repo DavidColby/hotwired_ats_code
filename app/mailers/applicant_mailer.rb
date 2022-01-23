a7d51378dd7913e9c3b8720d24c1c8f70d2b571e
David Colby <davidlcolby@gmail.com>
2022-01-23 17:34:25 -0600

    Properly set from address on outbound applicant emails

--- app/mailers/applicant_mailer.rb
+++ app/mailers/applicant_mailer.rb
@@ -6,7 +6,7 @@ class ApplicantMailer < ApplicationMailer
 
     mail(
       to: @applicant.email,
-      from: @user.email,
+      from: "reply-#{@user.email_alias}@hotwiringrails.com",
       subject: @email.subject
     )
   end
